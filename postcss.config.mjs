cd3852dc5627c5cae16f7968c942095b08c60a69
mariopi <mariopi.wac@gmail.com>
2025-11-13 15:23:35 +0700

    revisi package.lock,json

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss').Config} */
 const config = {
   plugins: {
-    'tailwindcss': {}, // <-- Ini adalah plugin v3 yang benar
+    'tailwindcss': {},
     'autoprefixer': {},
   },
 };
